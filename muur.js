7edde34f7f4293c2f819446b4308521d615ffcc0
Jan Pieter Waagmeester <jieter@jieter.nl>
2015-09-29 20:30:13 +0200

    Randomized versie van van het kruisverband

--- muur.js
+++ muur.js
@@ -1,9 +1,9 @@
 var container = d3.select('body');
 
 var margin = {
-    left: 40,
+    left: 20,
     right: 20,
-    top: 0,
+    top: 20,
     bottom: 40
 };
 // var width = function () { return container.offsetWidth; };
@@ -13,24 +13,29 @@ var outerHeight = Math.floor(outerWidth() * (2 / 7));
 var height = outerHeight - margin.top - margin.bottom;
 
 
-function plot(naam, muur, size) {
+function plot(naam, muur, size, distribution) {
     d3.select('body').append('h2').text(naam);
     var svg = container.append('svg').attr({
         width: outerWidth(),
         height: outerHeight
-    }).append('g')
-        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    })
+
+    var wall = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    var distr = svg.append('g').attr('transform', 'translate(10, ' + margin.top + ')');
 
     var x = d3.scale.linear().range([width(), 0]);
     var y = d3.scale.linear().range([10, height]);
+    // var rx = d3.scale.linear().range([60, 0]);
 
     var axis = {
         x: d3.svg.axis().scale(x).orient('bottom'),
         y: d3.svg.axis().scale(y).orient('left')
+        // rx: d3.svg.axis().scale(rx).orient('bottom').ticks(3)
     }
 
     x.domain([700, 0]);
     y.domain([205, 0]);
+    // rx.domain([0.4, 0]);
 
     function transform (input) {
         var out = [];
@@ -38,14 +43,14 @@ function plot(naam, muur, size) {
         input.forEach(function (row, rowid) {
             var rowY = size.dikte + rowid * (size.lagenmaat);
             var rowX = 0;
-            console.log(row, rowid);
             row.forEach(function (b) {
                 var s = size.stones[b];
 
                 out.push({
                     x: rowX,
                     y: rowY,
-                    width: s.w
+                    width: s.w,
+                    class: s.name
                 });
                 rowX += s.dx;
             });
@@ -54,13 +59,12 @@ function plot(naam, muur, size) {
         return out;
     }
     var data = transform(muur);
-    console.log(data);
 
     var graph_hoogte = height - y(size.dikte);
 
     var brick = function (selection) {
         selection.append('rect').attr({
-            class: 'brick',
+            class: function (d) { return 'brick ' + d.class; },
             x: function (d) { return x(d.x); },
             y: function (d) { return y(d.y); },
             width: function (d) { return x(d.width); },
@@ -68,7 +72,7 @@ function plot(naam, muur, size) {
         });
     };
 
-    var bricks = svg.append('g').attr('class', 'bricks');
+    var bricks = wall.append('g').attr('class', 'bricks');
 
     var selection = bricks.selectAll('.brick')
         .data(data)
@@ -77,9 +81,23 @@ function plot(naam, muur, size) {
 
 
     for (var key in axis) {
-        var el = svg.append('g').attr('class', 'axis ' + key).call(axis[key]);
-        if (key == 'x') {
+        var parent = key == 'rx' ? distr : wall;
+        var el = parent.append('g').attr('class', 'axis ' + key).call(axis[key]);
+        if (key == 'x' || key == 'rx') {
             el.attr('transform', 'translate(0, ' + height + ')')
         }
     }
+
+    var line = function (sel) {
+        sel.attr('d', d3.svg.line()
+            .x(function (d) { return rx(d[0]); })
+            .y(function (d) { return y(d[1]); })
+        );
+    };
+
+    var sel = distr.selectAll('.plot').data([distribution]);
+
+    // console.log(distribution);
+    // sel.enter()
+    //     .append('path').attr('class', 'plot').call(line);
 }
